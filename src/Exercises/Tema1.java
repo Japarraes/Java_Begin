c20d5af46b3fd4a7c253abbfe2fa6d957666db3f
Japarraes <110259395+Japarraes@users.noreply.github.com>
2022-08-10 10:13:13 +0200

    Add files via upload

---
+++ src/Exercises/Tema1.java
@@ -0,0 +1,35 @@
+package Exercises;
+
+public class Tema1 {
+
+    public static void main(String[] args) {
+
+        // Tipos de variables
+        byte var1 = 0;
+        short var2 = 1;
+        int var3 = 2;
+        long var4 = 30000000;
+        float var5 = 5.234f;
+        double var6 = 6.789;
+        char var7 = 'a';
+        String var8 = "abc";
+        boolean var9 = false;
+        boolean var10 = true;
+
+        System.out.println("byte :" + var1);
+        System.out.println("short :" + var2);
+        System.out.println("int :" + var3);
+        System.out.println("long :" + var4);
+        System.out.println("float :" + var5);
+        System.out.println("double :" + var6);
+        System.out.println("char :" + var7);
+        System.out.println("String :" + var8);
+        System.out.println("boolean :" + var9);
+        System.out.println("boolean :" + var10);
+
+
+
+
+
+    }
+}
